281f54a014ab212ad11ba85b012499e4740d55ea
Aadi2005123 <129484451+Aadi2005123@users.noreply.github.com>
2026-08-07 14:16:32 +0530

    Time: 24 ms (36.08%) | Memory: 13.6 MB (18.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if len(s) != len(t):
+        if len(s)!=len(t):
             return False
-
-        return sorted(s) == sorted(t)
+        return sorted(s)==sorted(t)
